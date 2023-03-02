36ffc6bc4069c1d746fb40fe423f941a2a1b47a8
saziz <shan.azizh@gmail.com>
2023-03-02 21:11:18 +0100

    feat: retrieve list of seances

---
+++ src/app/model/models.ts
@@ -0,0 +1,51 @@
+export interface User {
+  id: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  dateOfBirth: Date;
+}
+
+export interface JwtToken {
+  token: string;
+}
+
+export interface UserResponse {
+  firstname: string;
+  lastname: string;
+  email: string;
+}
+
+export interface RegisterDto {
+  email: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+  dateOfBirth: string;
+  role: string;
+}
+
+export interface Seance {
+  id: string;
+  name: string;
+  description : string;
+  rewardPoints: number;
+  dateStart: Date;
+  dateEnd: Date;
+  coach: UserResponse;
+  participants: UserResponse[];
+}
+
+export interface SeanceList {
+  id: string;
+  name: string;
+  description : string;
+  rewardPoints: number;
+  dateStart: Date;
+  dateEnd: Date;
+  coach: UserResponse;
+}
+
+export enum Role {
+  Collaborator = 'COLLABORATOR',
+}
