8c721bcd227eba3ec0ab4e0fbea6f3112109fec2
saziz <shan.azizh@gmail.com>
2023-03-02 21:10:56 +0100

    feat: login and register

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,7 @@ export class LoginComponent implements OnInit {
 
   loginForm: any;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.initUserForm();
@@ -26,6 +28,18 @@ export class LoginComponent implements OnInit {
   signIn() {
     if(!this.loginForm.invalid) {
       console.log("userform: ", this.loginForm.value);
+      const data = {
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password
+      }
+      this.authService.authenticate(data.email, data.password).subscribe({
+        next: () => {
+          this.router.navigate(['home']).then();
+        },
+        error: () => {
+          console.log("une erreur est survenue");
+        }
+      });
     }
   }
 
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { tap } from 'rxjs';
+import {JwtToken, RegisterDto} from "../model/models";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private _token: JwtToken | null = AuthService.getToken();
+
+  constructor(private api: HttpClient) {
+    // TODO: for disconnect when token expires
+    // this.api.get<User>('auth/me', { withCredentials: true }).subscribe({
+    //   next: (user) => {
+    //     this._user = user;
+    //     AuthService.storeUser(user);
+    //   },
+    //   error: () => {
+    //     this._user = null;
+    //     localStorage.removeItem('user');
+    //   },
+    // });
+  }
+
+  get token(): JwtToken {
+    return Object.assign({}, this._token);
+  }
+
+  get isAuthenticated(): boolean {
+    return this._token != null;
+  }
+
+  static getToken(): JwtToken | null {
+    const token = localStorage.getItem('token');
+    return token ? JSON.parse(token) : null;
+  }
+
+  private static storeToken(token: JwtToken) {
+    localStorage.setItem('token', JSON.stringify(token));
+  }
+
+  authenticate(email: string, password: string) {
+    return this.api
+      .post<JwtToken>('auth/authenticate', { email, password },)
+      .pipe(
+        tap((token) => {
+          this._token = token;
+          AuthService.storeToken(token);
+        }),
+      );
+  }
+
+  register(data: RegisterDto) {
+    console.log(data);
+    return this.api
+      .post<JwtToken>('auth/register', data)
+      .pipe(
+        tap((user) => {
+          this._token = user;
+          AuthService.storeToken(user);
+        }),
+      );
+  }
+}
