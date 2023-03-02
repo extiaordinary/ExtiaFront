83feaaabcb54d440bf36daf38d4503e0c0149c1a
Mouss <diarra940.moustapha@gmail.com>
2023-03-02 10:10:30 +0100

    footer ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ProfilComponent } from './components/profil/profil.component';
+import {FooterComponent} from "./components/footer/footer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    NavBarComponent,
-    NavBarComponent,
-    ProfilComponent
-  ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        NavBarComponent,
+        FooterComponent,
+      ProfilComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
