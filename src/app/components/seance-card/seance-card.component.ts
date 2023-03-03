5710844c185894da3e46ee6e774404ba7845336e
saziz <shan.azizh@gmail.com>
2023-03-03 12:01:23 +0100

    draft: seance card

--- src/app/components/seance-card/seance-card.component.ts
+++ src/app/components/seance-card/seance-card.component.ts
@@ -1,22 +1,48 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {SeanceList} from "../../model/models";
 import {SeanceService} from "../../services/seance.service";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-seance-card',
   templateUrl: './seance-card.component.html',
   styleUrls: ['./seance-card.component.css']
 })
-export class SeanceCardComponent implements OnInit {
+export class SeanceCardComponent {
   @Input() seanceCard: SeanceList | null = null;
 
-  constructor(private seanceService: SeanceService) { }
+  isInSeance = false;
+  isCoach = false;
+
+  constructor(private seanceService: SeanceService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    if (this.seanceCard?.id && this.authService.isAuthenticated){
+      console.log(this.seanceCard);
+      this.seanceService.IsUserJoinedSeance(this.seanceCard.id).subscribe(
+        (isInSeance) => {
+          if(this.seanceCard){
+            this.isInSeance = isInSeance.isCollaborator;
+            this.isCoach = isInSeance.isCoach;
+          }
+        }
+      )
+    }
   }
 
-  isUserRegisteredOnSeance() {
-    return this.seanceCard?.isUserInSeance;
+  showRegisterToSeance(){
+    console.log("show register" + this.seanceCard?.name + " " + this.isInSeance);
+    if(!this.isCoach){
+      return !this.isInSeance;
+    }
+    return false;
+  }
+
+  showCancelRegisterToSeance(){
+    if(!this.isCoach){
+      return this.isInSeance;
+    }
+    return false;
   }
 
   registerOnSeance() {
@@ -24,9 +50,12 @@ export class SeanceCardComponent implements OnInit {
       this.seanceService.joinSeance(this.seanceCard.id).subscribe(
         () => {
           console.log("Vous avez rejoins la seance");
+          if(this.seanceCard){
+            this.isInSeance = true;
+          }
         },
-        () => {
-          console.log("Une erreur est survenue");
+        (err) => {
+          console.log("Une erreur est survenue" + err.toString());
         }
       )
     }
@@ -35,6 +64,7 @@ export class SeanceCardComponent implements OnInit {
   cancelRegistrationOnSeance() {
     // this.isRegisteredOnSeance = !this.isRegisteredOnSeance;
     //call API to cancel registration on seance
+
     console.log("TODO");
   }
 
